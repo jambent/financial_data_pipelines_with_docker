488ca9edb3c35e57650343fd0108171bb233c894
jwbennett <jwbennett@Laptop>
2024-02-04 13:55:14 +0000

    Adds RDS creation using local variables from .tfvars file

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,3 +1,13 @@
+locals{
+    port = var.db_credentials.db_credentials.port
+    database = var.db_credentials.db_credentials.database
+    username = var.db_credentials.db_credentials.username
+    password = var.db_credentials.db_credentials.password
+}
+
+variable "db_credentials"{}
+
+
 resource "aws_secretsmanager_secret" "db_creds_val_data" {
   name = "db_credentials_val_data"
   recovery_window_in_days = 0
@@ -5,5 +15,5 @@ resource "aws_secretsmanager_secret" "db_creds_val_data" {
 
 resource "aws_secretsmanager_secret_version" "db_creds_val_data" {
   secret_id     = aws_secretsmanager_secret.db_creds_val_data.id
-  secret_string = var.db_credentials_val_data
+  secret_string = "{\"port\":\"${local.port}\", \"database\":\"${local.database}\",\"username\":\"${local.username}\",\"password\":\"${local.password}\"}"
 }
