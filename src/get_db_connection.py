28a43782bad3f40dbb26da1760e370166ffc55d0
jwbennett <jwbennett@Laptop>
2024-02-08 19:27:39 +0000

    Obtained working connection to RDS instance by using ssl_context argument

--- src/get_db_connection.py
+++ src/get_db_connection.py
@@ -1,5 +1,5 @@
 from pg8000.native import Connection, DatabaseError
-#from pg8000 import dbapi
+import ssl
 from get_db_credentials import get_db_credentials
 
 
@@ -10,14 +10,10 @@ def get_db_connection(db_credentials):
         DB_USER = db_credentials['username']
         DB_DB = db_credentials['database']
         DB_PASSWORD = db_credentials['password']
-        print(DB_ENDPOINT)
-        print(DB_USER)
-        print(DB_DB)
-        print(DB_PASSWORD)
-        # return(dbapi.connect(host=DB_ENDPOINT, user=DB_USER, 
-        #                     password=DB_PASSWORD,database=DB_DB))
+
+        ssl_context = ssl.SSLContext()
         return (Connection(host=DB_ENDPOINT, user=DB_USER, 
-                            password=DB_PASSWORD,database=DB_DB,port=5432))
+                            password=DB_PASSWORD,database=DB_DB,ssl_context=ssl_context))
     except DatabaseError as e:
         print('Error connecting to database')
         raise e
@@ -25,7 +21,12 @@ def get_db_connection(db_credentials):
 
 if __name__ == '__main__':
     creds = get_db_credentials("db_credentials_val_data")
-    #print(creds)
     conn = get_db_connection(creds)
-    print(conn)
-    conn.close()
+    try:
+        query = """CREATE TABLE stores (
+        store_id SERIAL PRIMARY KEY,
+        city VARCHAR
+        );"""
+        conn.run(query)
+    finally:
+        conn.close()
